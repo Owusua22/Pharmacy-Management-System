9104e78ddafa8096a81bbb091999b3ed3ba979b9
Sarah Nkansah <108684372+Owusua22@users.noreply.github.com>
2024-06-18 13:03:44 -1200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,26 +12,34 @@ const customerRoutes = require('./routes/customerRoute');
 
 dotenv.config();
 
+const app = express();
+
+// Middleware setup
+app.use(express.json());
+app.use(cors({
+  origin: '*', // Adjust as needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Connect to database
 connectDB();
 
-
-const app = express();
-app.use(express.json());
-app.get("/",(req, res) =>{
-  res.send("Backend is working");
-})
-
-// Use CORS middleware
-app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+// Define routes
 app.use('/api/users', userRoutes);
 app.use('/api/inventory', inventoryRoutes);
 app.use('/api/sales', salesRoutes);
 app.use('/api/prescriptions', prescriptionRoutes);
 app.use('/api/customers', customerRoutes);
 
-const PORT = process.env.PORT || 5000;
+// Base route
+app.get("/", (req, res) => {
+  res.send("Backend is working");
+});
 
+// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
